3f81968c8841d74f35cd024523f8dba50a3ef922
btpgy54 <t410y831@yahoo.co.jp>
2022-03-25 21:13:02 +0900

    Issue6

--- app/controllers/calendars_controller.rb
+++ app/controllers/calendars_controller.rb
@@ -19,7 +19,7 @@ class CalendarsController < ApplicationController
   end
 
   def get_week
-    week_days = ['(日)','(月)','(火)','(水)','(木)','(金)','(土)']
+    wdays = ['(日)','(月)','(火)','(水)','(木)','(金)','(土)']
 
     # Dateオブジェクトは、日付を保持しています。下記のように`.today.day`とすると、今日の日付を取得できます。
     @todays_date = Date.today
@@ -36,16 +36,14 @@ class CalendarsController < ApplicationController
       end
 
 
-      wday_num = Date.today.wday
-      if wday_num >=7
+      wday_num = Date.today.wday + x
+      if wday_num >= 7
         wday_num = wday_num -7
       end
 
-      days = { month:  (@todays_date + x).month, date: (@todays_date+x).day, plans:  today_plans, wday:  wdays[wday_num]}
+      days = { month:  (@todays_date + x).month, date:  (@todays_date + x).day, plans:  today_plans, wday:  wdays[wday_num] }
       @week_days.push(days)
     end
-
-
 
   end
 end
